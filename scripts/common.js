3bf8a96702be75075a8c571f9777258509e4abd7
Roman <skrypak1997@gmail.com>
2020-02-26 11:17:51 +0200

    Main task done

--- scripts/common.js
+++ scripts/common.js
@@ -1,12 +1,34 @@
 var arr=['hello', 'world', 'Kiev', 'Kharkiv', 'Odessa', 'Lviv'];
 function f(arr) {
+    let n = arr.length
 
 
-for(let i=0; i<6; i++)
+for(let i=0; i<n; i++)
     {
-        let liel=document.createElement('li');
-        liel.innerHTML = arr[i];
-        list.append(liel);
+        if(typeof (arr[i])=='object'){
+            return f(arr[i])
+        }
+        else {
+            let liel = document.createElement('li');
+            liel.innerHTML = arr[i];
+            list.append(liel);
+        }
     }
 }
+
+
 f(arr);
+
+let t = setInterval (function ()
+{
+    function f (x) {return (x / 100).toFixed (2).substr (2)}
+    let o = document.getElementById ('timer'), s = o.innerHTML;
+    s--;
+    if (s < 1) {
+        sec.remove()
+        timer.remove()
+        list.remove()
+    }
+        //s = o.getAttribute ('long');
+    o.innerHTML = f(s);
+}, 1000);
