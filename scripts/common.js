f7dd7334dc0845076a230cc526ad13edbce38f8e
Roman <skrypak1997@gmail.com>
2020-03-10 21:43:19 +0200

    uppdate

--- scripts/common.js
+++ scripts/common.js
@@ -1,23 +1,15 @@
-var arr=['hello', 'world', 'Kiev', 'Kharkiv', 'Odessa', 'Lviv'];
-function f(arr) {
-    let n = arr.length
+const arr=['hello', 'world', 'Kiev', 'Kharkiv', 'Odessa', 'Lviv'];
 
+function list(arr){
 
-for(let i=0; i<n; i++)
-    {
-        if(typeof (arr[i])=='object'){
-            return f(arr[i])
-        }
-        else {
-            let liel = document.createElement('li');
-            liel.innerHTML = arr[i];
-            list.append(liel);
-        }
-    }
+    let listt = arr.map(el => `<li>${el}</li>`)
+    listt = `<ul>${listt.join("")}</ul>`
+    document.body.innerHTML +=listt
 }
 
 
-f(arr);
+list(arr)
+
 
 let t = setInterval (function ()
 {
@@ -29,6 +21,5 @@ let t = setInterval (function ()
         timer.remove()
         list.remove()
     }
-        //s = o.getAttribute ('long');
     o.innerHTML = f(s);
 }, 1000);
